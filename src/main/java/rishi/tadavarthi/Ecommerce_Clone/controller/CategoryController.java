f9039f2646de7aac91f15b6b6a119662bd8fe991
tadavarthi-rishi <rtadavar@asu.edu>
2025-06-04 21:31:24 -0400

    created controller, repo, serviceimpl and entity

--- src/main/java/rishi/tadavarthi/Ecommerce_Clone/controller/CategoryController.java
+++ src/main/java/rishi/tadavarthi/Ecommerce_Clone/controller/CategoryController.java
@@ -1,19 +1,24 @@
 package rishi.tadavarthi.Ecommerce_Clone.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import rishi.tadavarthi.Ecommerce_Clone.io.CategoryRequest;
 import rishi.tadavarthi.Ecommerce_Clone.io.CategoryResponse;
+import rishi.tadavarthi.Ecommerce_Clone.service.CategoryService;
 
 @RestController
 @RequestMapping("/categories")
+@RequiredArgsConstructor
 public class CategoryController {
 
+    private final CategoryService categoryService;
+
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CategoryResponse addCategory(@RequestBody CategoryRequest request){
 
+        return categoryService.add(request);
     }
 
 }
