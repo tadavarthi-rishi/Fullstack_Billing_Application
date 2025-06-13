348a7ccd4efe4c7b484446520f52fe83e39a334e
tadavarthi-rishi <rtadavar@asu.edu>
2025-06-12 21:58:19 -0400

    implemented categoryentity and service

--- src/main/java/rishi/tadavarthi/Ecommerce_Clone/entity/CategoryEntity.java
+++ src/main/java/rishi/tadavarthi/Ecommerce_Clone/entity/CategoryEntity.java
@@ -1,8 +1,10 @@
 package rishi.tadavarthi.Ecommerce_Clone.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,6 +14,8 @@ import java.sql.Timestamp;
 @Table(name="tbl_category")
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/rishi/tadavarthi/Ecommerce_Clone/service/CategoryService.java
+++ src/main/java/rishi/tadavarthi/Ecommerce_Clone/service/CategoryService.java
@@ -3,6 +3,9 @@ package rishi.tadavarthi.Ecommerce_Clone.service;
 import rishi.tadavarthi.Ecommerce_Clone.io.CategoryRequest;
 import rishi.tadavarthi.Ecommerce_Clone.io.CategoryResponse;
 
+import java.util.List;
+
 public interface CategoryService {
     CategoryResponse add(CategoryRequest request);
+    List<CategoryResponse> read();
 }
