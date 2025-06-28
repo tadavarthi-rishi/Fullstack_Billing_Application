773b2b0d0e43d27fb062c63ea3681dfde66918ae
tadavarthi-rishi <rtadavar@asu.edu>
2025-06-28 11:17:56 -0400

    created categoryForm with name,description,bgcolor and submit button

--- client/src/components/CategoryForm/CategoryForm.jsx
+++ client/src/components/CategoryForm/CategoryForm.jsx
@@ -11,6 +11,32 @@ const CategoryForm = () => {
                                 </label>
                                 <input type="file" name="image" id="image" className='form-control' hidden />
                             </div>
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label">Name</label>
+                                <input type="text"
+                                       name="name"
+                                       id="name"
+                                       className="form-control"
+                                       placeholder="Enter Category Name" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="description" className="form-label">Description</label>
+                                <textarea
+                                        rows="5"
+                                       name="description"
+                                       id="description"
+                                       className="form-control"
+                                        placeholder="Write content here..."></textarea>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="bgcolor" className="form-label">Background color</label>
+                                <br/>
+                                <input type="color"
+                                       name="bgcolor"
+                                       id="bgcolor"
+                                       placeholder={"#ffffff"}/>
+                            </div>
+                            <button type="submit" className="btn btn-primary w-100">Submit</button>
                         </form>
                     </div>
                 </div>
