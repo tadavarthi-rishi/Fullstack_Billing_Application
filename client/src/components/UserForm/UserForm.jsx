a44c903695dbde0830a78488147275f1481ef9ac
tadavarthi-rishi <rtadavar@asu.edu>
2025-07-27 22:01:13 -0400

    created functionality to list and delete users with admin access

--- client/src/components/UserForm/UserForm.jsx
+++ client/src/components/UserForm/UserForm.jsx
@@ -1,17 +1,60 @@
-const UserForm = () => {
+import {useState} from "react";
+import {addUser} from "../../service/UserService.js";
+import toast from "react-hot-toast";
+
+const UserForm = ({setUsers}) => {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        role: 'ROLE_USER'
+    });
+
+    const onChangeHandler = (e) => {
+        const value = e.target.value;
+        const name = e.target.name;
+        setData((data) => ({...data, [name]: value}));
+    }
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        try{
+            const response = await addUser(data);
+            setUsers((prevUsers) => [...prevUsers, response.data]);
+            toast.success("User added successfully!");
+            setData({
+                name: "",
+                email: "",
+                password: "",
+                role: "ROLE_USER",
+            });
+        } catch (e) {
+            console.error(e);
+            toast.error("Unable to add user");
+        } finally {
+            setLoading(false);
+        }
+    }
+
     return (
         <div className="mx-2 mt-2">
             <div className="row">
-                <div className="card col-md-8 form-container">
+                <div className="card col-md-12 form-container">
                     <div className = "card-body">
-                        <form>
+                        <form onSubmit = {onSubmitHandler}>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Name</label>
                                 <input type="text"
                                        name="name"
                                        id="name"
                                        className="form-control"
-                                       placeholder="Virat Kohli" />
+                                       placeholder="Virat Kohli"
+                                       onChange = {onChangeHandler}
+                                       value = {data.name}
+
+                                />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className="form-label">Email</label>
@@ -19,7 +62,10 @@ const UserForm = () => {
                                        name="email"
                                        id="email"
                                        className="form-control"
-                                       placeholder="yourname@amazon.com" />
+                                       placeholder="yourname@amazon.com"
+                                       onChange = {onChangeHandler}
+                                       value = {data.email}
+                                />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="password" className="form-label">Password</label>
@@ -27,10 +73,14 @@ const UserForm = () => {
                                        name="password"
                                        id="password"
                                        className="form-control"
-                                       placeholder="************" />
+                                       placeholder="************"
+                                       onChange = {onChangeHandler}
+                                       value = {data.password}
+                                />
                             </div>
-
-                            <button type="submit" className="btn btn-warning w-100">Submit</button>
+                            <button type="submit" className="btn btn-warning w-100" disabled={loading}>
+                                {loading ? "Loading...": "Save"}
+                            </button>
                         </form>
                     </div>
                 </div>
--- client/src/components/UsersList/UsersList.jsx
+++ client/src/components/UsersList/UsersList.jsx
@@ -1,6 +1,69 @@
-const UsersList = () => {
+import {useState} from "react";
+import {deleteUser} from "../../service/UserService.js";
+import toast from "react-hot-toast";
+
+const UsersList = ({users, setUsers}) => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const filteredUsers = users.filter(user =>
+
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+
+    );
+
+    const deleteByUserId =  async (id) => {
+        try{
+                await deleteUser(id);
+                setUsers(prevUsers => prevUsers.filter(user => user.userId !== id));
+                toast.success("User deleted successfully");
+        } catch (e){
+        console.error(e);
+        toast.error("Unable to delete user");
+        }
+    }
+
     return (
-        <div>Users list</div>
+        <div className={"category-list-container"} style = {{height:'100vh', overflowY:'auto', overflowX:'hidden'}}>
+            <div className = "row pe-2">
+                <div className = "input-group mb-3">
+                    <input type = "text"
+                           name={"keyword"}
+                           placeholder = "search by keywoard"
+                           className = "form-control"
+                           onChange={(e) => {setSearchTerm(e.target.value)}}
+                           value = {searchTerm}
+
+                    />
+                    <span className = "input-group-text bg-warning">
+                        <i className = "bi bi-search">
+
+                        </i>
+                    </span>
+                </div>
+            </div>
+            <div className = "row g-3 pe-2">
+                {
+                    filteredUsers.map((user, index) => (
+                        <div key = {index} className = "col-12">
+                            <div className="card p-3 bg-dark">
+                                <div className="d-flex align-items-center">
+                                    <div className="flex-grow1">
+                                        <h5 className="mb-1 text-white">{user.name}</h5>
+                                        <p className="mb-0-text-white">
+                                            {user.email}
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <button className="btn btn-danger btn-sm" onClick={() => deleteByUserId(user.userId)}>
+                                            <i className="bi bi-trash"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
     )
 }
 
