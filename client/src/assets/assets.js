8a104c98213f8e10c0010cb20e7c2bd68e0b19d4
tadavarthi-rishi <rtadavar@asu.edu>
2025-08-05 21:51:25 -0400

    created dropdown for profile settings and logout from account

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,8 +1,10 @@
 import logo from './logo.png';
 import upload from './upload.png';
 import login from './Login-bp.jpg';
+import profile from './profile.png';
 export const assets = {
     logo,
     upload,
-    login
+    login,
+    profile
 }
--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -2,6 +2,11 @@ import './Menubar.css';
 import {assets} from "../../assets/assets.js";
 import {Link} from "react-router-dom";
 const Menubar = () => {
+
+    const logout = () => {
+
+    }
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
             <a className="navbar-brand" href="#">
@@ -30,6 +35,29 @@ const Menubar = () => {
                     </li>
                 </ul>
                 {/* Add the dropdown for userprofile*/}
+                <ul className="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+                    <li className="nav-item dropdown">
+                        <a href="#" className="nav-link dropdown-toggle" id={"navbarDropdown"} role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src={assets.profile} alt="" height = {32} width={32}/>
+                        </a>
+                        <ul className="dropdown-menu dropdown-menu-end" aria-labelledby={"navbarDropdown"}>
+                            <li>
+                                <a href="#!" className="dropdown-item">
+                                    Settings
+                                </a>
+                                <a href="#!" className="dropdown-item">
+                                    Activity Log
+                                </a>
+                                    <li>
+                                        <hr className="dropdown-divider"/>
+                                    </li>
+                                <a href="#!" className="dropdown-item" onClick = {logout}>
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
