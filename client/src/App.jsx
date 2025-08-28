03bcecea4e04cee1b7a9891bc2b213c13b07d157
tadavarthi-rishi <rtadavar@asu.edu>
2025-08-27 21:18:53 -0400

    implemented protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,9 +8,31 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import {Toaster} from "react-hot-toast";
 import Login from "./pages/Login/Login.jsx";
 import OrderHistory from "./pages/OrderHistory/OrderHistory.jsx";
+import {useContext} from "react";
+import {AppContext} from "./Context/AppContext.jsx";
 
 const App = () => {
     const location = useLocation();
+    const {auth} = useContext(AppContext);
+
+    const  LoginRoute = ({element}) => {
+        if(auth.token){
+            return <Navigate to = "/dashboard" replace />;
+        }
+        return element;
+    }
+
+    const ProtectedRoute = ({element, allowedRoles}) => {
+        if(!auth.token){
+            return <Navigate to = "/login" replace />;
+        }
+
+        if(allowedRoles && !allowedRoles.includes(auth.role)) {
+            return <Navigate to = "/dashboard" replace />;
+        }
+        return element;
+    }
+
     return (
         <div>
             {location.pathname !== "/login" && <Menubar />}
